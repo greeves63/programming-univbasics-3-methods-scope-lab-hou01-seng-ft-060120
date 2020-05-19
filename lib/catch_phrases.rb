a9061ee611705a2a9cbc635818180be3b8f396d7
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 12:28:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,13 @@
 def mario
-  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
-  puts "It's-a me, Mario!\n"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts "It's-a me, Mario!"
 end
 
 def toadstool
   puts status
   
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
 end
