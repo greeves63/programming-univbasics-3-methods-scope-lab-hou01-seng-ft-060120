542580d8f0b89e128c3f9af2a103631dacd5e2eb
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 12:26:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 def mario
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  puts "It's-a me, Mario!"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+  puts "It's-a me, Mario!\n"
 end
 
 def toadstool
